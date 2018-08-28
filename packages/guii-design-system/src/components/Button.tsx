49c0a68ebfc240abdf7a559dd0a34a456bb6280d
thisjeremiah <jeremiahmontoya@me.com>
2018-08-28 01:17:07 -0700

    Add Button component

---
+++ packages/guii-design-system/src/components/Button.tsx
@@ -0,0 +1,58 @@
+import { Touchable } from 'react-primitives'
+import styled from 'styled-components'
+import ss from 'styled-system'
+import { ExtractProps } from '../utils'
+
+type TouchableProps = ExtractProps<typeof Touchable>
+
+export interface ButtonProps
+  extends ss.FontSizeProps,
+    ss.FontWeightProps,
+    ss.LineHeightProps,
+    ss.SpaceProps,
+    ss.ColorProps,
+    ss.SizeProps,
+    ss.BorderRadiusProps,
+    ss.BorderProps,
+    TouchableProps {}
+
+export const Button = styled<ButtonProps>(Touchable)`
+  ${ss.fontSize};
+  ${ss.fontWeight};
+  ${ss.lineHeight};
+  ${ss.space};
+  ${ss.color};
+  ${ss.borderRadius};
+  ${ss.border};
+  ${ss.size};
+
+  font-family: inherit;
+  webkit-font-smoothing: antialiased;
+  display: inline-block;
+  vertical-align: middle;
+  text-align: center;
+  text-decoration: none;
+  appearance: none;
+  &:disabled {
+    opacity: 1/4;
+  }
+  &:focus {
+    outline: none;
+    box-shadow: 0 0 0 2px ${props => props.theme.colors[props.bg as string]};
+  }
+`
+
+Button.defaultProps = {
+  fontSize: 1,
+  fontWeight: 'bold',
+  lineHeight: 16 / 14,
+  m: 0,
+  px: 3,
+  py: 2,
+  color: 'white',
+  bg: 'blue',
+  borderRadius: 2,
+  border: 0,
+}
+
+Button.displayName = 'Button'
